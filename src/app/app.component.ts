89ff6db02a0b97e10c9b554e663d35ac404342b1
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-02-29 10:51:29 -0800

    Make app component standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
+import { CourseCardComponent } from "./course-card/course-card.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  standalone: true,
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  imports: [CourseCardComponent],
 })
-export class AppComponent {
-
-
-
-}
+export class AppComponent {}
